c1fe36cfd8f8faf32c63f83bddb24eee98cbe06c
남정욱 <wjddnr3304@gmail.com>
2024-11-12 13:04:14 +0900

    select 문 쿼리

--- dbtest/Program.cs
+++ dbtest/Program.cs
@@ -13,11 +13,17 @@ namespace dbtest
         {
             SqlConnection conn = new SqlConnection("Data Source = .; Initial Catalog = test_db; Integrated Security = True");
             conn.Open();
-            string query = "update sales set itemcode = '노트북' where itemcode = '컴퓨터'"; // 수정 쿼리
+            string query = "select * from sales";
+                //"update sales set itemcode = '노트북' where itemcode = '컴퓨터'"; // 수정 쿼리
                 //"insert into sales(salecode,customercode,itemcode,data,qty,price) values  (1, '홍길동', '컴퓨터', '2024-08-02', 10, 10000)";  // 등록 쿼리
             SqlCommand cmd = new SqlCommand(query, conn);
-            cmd.ExecuteNonQuery();
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                Console.WriteLine(r[0].ToString() + "," + r[1].ToString() + "," + r[2].ToString() + "," + r[3].ToString() + "," + r[4].ToString() + "," + r[5].ToString());
+            }
+            //cmd.ExecuteNonQuery();
             conn.Close();
         }
-    }
+    }    
 }
