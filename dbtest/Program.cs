38511040c2212542a812d21f16c6b67fca2ad376
남정욱 <wjddnr3304@gmail.com>
2024-11-12 11:39:06 +0900

    수정 쿼리문 작성

--- dbtest/Program.cs
+++ dbtest/Program.cs
@@ -13,12 +13,11 @@ namespace dbtest
         {
             SqlConnection conn = new SqlConnection("Data Source = .; Initial Catalog = test_db; Integrated Security = True");
             conn.Open();
-            string query = "insert into sales(salecode,customercode,itemcode,data,qty,price) values  (1, '홍길동', '컴퓨터', '2024-08-02', 10, 10000)";
+            string query = "update sales set itemcode = '노트북' where itemcode = '컴퓨터'"; // 수정 쿼리
+                //"insert into sales(salecode,customercode,itemcode,data,qty,price) values  (1, '홍길동', '컴퓨터', '2024-08-02', 10, 10000)";  // 등록 쿼리
             SqlCommand cmd = new SqlCommand(query, conn);
             cmd.ExecuteNonQuery();
             conn.Close();
-
-
         }
     }
 }
